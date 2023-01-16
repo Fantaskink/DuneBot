ca10865855d5d39f5efe7c2760cc88ee129783d0
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2023-01-16 15:18:56 +0100

    Created csv helper function

--- DuneBot/csv_helper.py
+++ DuneBot/csv_helper.py
@@ -3,13 +3,26 @@ import csv
 def set_subreddit(channel, subreddit):
     print(channel, subreddit)
 
-    index = 0
+    # Open the csv file for reading and writing
+    with open("subreddits.csv", "r+") as csv_file:
+        # Read all the rows into a list
+        rows = csv_file.readlines()
 
-    with open('subreddits.csv', 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
+        # Iterate through each row
+        for i, row in enumerate(rows):
+            # Split the row into a list
+            row_data = row.strip().split(",")
 
-        for row in csv_reader:
-            if channel in row or subreddit in row:
-                index = row.index
+            # Check if the row contains either of the strings
+            if channel in row_data or subreddit in row_data:
+                # Overwrite the row with new data
+                rows[i] = channel + "," + subreddit + "\n"
+        
+        # Clear the file
+        csv_file.seek(0)
+        csv_file.truncate()
+        
+        # Write the rows back to the file
+        csv_file.writelines(rows)
 
 
