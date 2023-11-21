fc83994a63a34c0d1883ae7614c3318cf5af2088
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2023-11-21 10:39:42 +0100

    Rewrite primary color finder

--- DuneBot/primarycolor.py
+++ DuneBot/primarycolor.py
@@ -1,30 +1,15 @@
-from PIL import Image
 import requests
-import numpy as np
 from io import BytesIO
+from colorthief import ColorThief
 
 def get_primary_hex_color(image_url):
     response = requests.get(image_url)
-    image = Image.open(BytesIO(response.content))
 
-    # Resize the image to a small size for faster processing (e.g., 100x100)
-    #resized_image = image.resize((100, 100))
-    
-    # Convert the image to a NumPy array
-    img_array = np.array(image)
-    
-    # Flatten the array to easily get color counts
-    flat_array = img_array.reshape(-1, img_array.shape[-1])
+    color_thief = ColorThief(BytesIO(response.content))
 
-    # Get the unique colors and their counts
-    colors, counts = np.unique(flat_array, axis=0, return_counts=True)
+    dominant_color = color_thief.get_color(quality=1)
 
-    # Get the index of the most frequent color
-    primary_color_index = np.argmax(counts)
-    
     # Convert the RGB values to hex
-    primary_color_rgb = colors[primary_color_index]
-    primary_hex = '#{:02x}{:02x}{:02x}'.format(*primary_color_rgb)
-
+    primary_hex = '#{:02x}{:02x}{:02x}'.format(*dominant_color)
     
     return int(primary_hex[1:], 16)
