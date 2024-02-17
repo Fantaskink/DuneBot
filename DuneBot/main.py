45cbb0dae45bcb0230d3ec537447274d4a0f4d6a
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-02-17 11:44:33 +0100

    Update main.py

--- DuneBot/main.py
+++ DuneBot/main.py
@@ -1157,7 +1157,7 @@ async def handle_boosters():
 
 @bot.tree.command(name="add_existing_booster")
 @app_commands.describe(user="Type in the name of the user you wish to add as a booster.", role_id="Type in the id of the role you wish to assign to the user.")
-async def add_existing_booster(interaction: discord.Interaction, user: discord.User, role_id: int):
+async def add_existing_booster(interaction: discord.Interaction, user: discord.User, role_id: str):
     if not interaction.user.guild_permissions.administrator:
         await interaction.response.send_message("You are not authorized to run this command.", ephemeral=True)
         return
