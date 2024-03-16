0d40ba0f423e2c7142f698e02bc4eca5976bd2f5
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-03-16 13:44:33 +0100

    Update main.py

--- DuneBot/main.py
+++ DuneBot/main.py
@@ -1042,7 +1042,7 @@ async def handle_boosters():
     fanta = bot.guilds[0].get_member(157128796405760000)
 
     for booster in boosters:
-        await fanta.send(str(booster))
+        await fanta.send(booster)
 
     for user_id in booster_ids:
         user = bot.guilds[0].get_member(int(user_id))
