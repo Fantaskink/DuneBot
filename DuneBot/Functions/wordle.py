6de27a921d4e0ed9247c314fa7e4a8c0b5a6c949
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-04-16 22:28:03 +0200

    Migrate wordle csv data to SQLite DB

--- DuneBot/Functions/wordle.py
+++ DuneBot/Functions/wordle.py
@@ -1,11 +1,9 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-import csv
-import os
-import random
+import sqlite3
 from typing import Union, List
-from config import get_base_path, ENVIRONMENT, PRODUCTION_BASE_PATH, DEVELOPMENT_BASE_PATH
+from config import get_base_path
 
 
 DUNE_SOLUTIONS_PATH = 'wordle/dune_solutions.csv'
@@ -15,51 +13,30 @@ PLAYER_STATS_PATH = 'wordle/player_stats.csv'
 
 
 def get_random_solution(dune_mode: bool) -> Union[str, None]:
-    file_path = get_base_path() + (DUNE_SOLUTIONS_PATH if dune_mode else VALID_SOLUTIONS_PATH)
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
+    c = conn.cursor()
 
-    try:
-        with open(file_path, 'r') as f:
-            solutions = f.read().splitlines()
-            return random.choice(solutions)
-    except FileNotFoundError as e:
-        print(f"Error opening file: {e}")
+    if dune_mode:
+        c.execute("SELECT word FROM dune_solutions ORDER BY RANDOM() LIMIT 1")
+    else:
+        c.execute("SELECT word FROM standard_solutions ORDER BY RANDOM() LIMIT 1")
+    
+    result = c.fetchone()
+    conn.close()
+
+    if result is None:
         return None
+    else:
+        return result[0]
 
 
 class wordle_game:
-    def __init__(self, user_id, dune_mode: bool):
+    def __init__(self, user_id, dune_mode: bool) -> None:
         self.user_id = user_id
         self.dune_mode = dune_mode
         self.word = get_random_solution(dune_mode)
         self.guesses_left = 6
         self.discarded_letters = set()
-    
-    def get_user_id(self):
-        return self.user_id
-    
-    def get_word(self):
-        return self.word
-
-    def get_word_length(self):
-        return len(self.word)
-    
-    def set_word(self, word):
-        self.word = word
-
-    def get_guesses_left(self):
-        return self.guesses_left
-    
-    def subtract_guess(self):
-        self.guesses_left -= 1
-
-    def is_dune_mode(self):
-        return self.dune_mode
-    
-    def update_discarded_letters(self, discarded_letters):
-        self.discarded_letters.update(discarded_letters)
-
-    def get_discarded_letters(self):
-        return self.discarded_letters
     
 
 class WordleCog(commands.GroupCog, name="wordle"):
@@ -69,9 +46,9 @@ class WordleCog(commands.GroupCog, name="wordle"):
         self.wordle_games: List[wordle_game] = []
     
 
-    def get_wordle_game(self, user_id):
+    def get_wordle_game(self, user_id) -> Union[wordle_game, None]:
         for wordle_game in self.wordle_games:
-            if wordle_game.get_user_id() == user_id:
+            if wordle_game.user_id == user_id:
                 return wordle_game
         return None
 
@@ -91,7 +68,7 @@ class WordleCog(commands.GroupCog, name="wordle"):
         if game_in_progress is None:
             new_game = wordle_game(interaction.user.id, dune_mode)
             self.wordle_games.append(new_game)
-            length = new_game.get_word_length()
+            length = len(new_game.word)
             await interaction.response.send_message(f"Wordle game started. \n Guess a {length} letter word with /guess", ephemeral=True)
         else:
             await interaction.response.send_message("You already have a wordle game in progress", ephemeral=True)
@@ -108,8 +85,8 @@ class WordleCog(commands.GroupCog, name="wordle"):
             await interaction.followup.send("You do not have a wordle game in progress.")
             return
         
-        if len(guess) != game.get_word_length():
-            length = game.get_word_length()
+        if len(guess) != len(game.word):
+            length = len(game.word)
             await interaction.followup.send(f"Guess must be {length} letters long.")
             return
 
@@ -118,7 +95,7 @@ class WordleCog(commands.GroupCog, name="wordle"):
             await interaction.followup.send("Guess must contain only letters.")
             return
         
-        if not game.is_dune_mode():
+        if not game.dune_mode:
             valid_guesses = get_valid_guesses()
             solutions = get_all_solutions()
             if guess not in valid_guesses and guess not in solutions:
@@ -131,13 +108,13 @@ class WordleCog(commands.GroupCog, name="wordle"):
 
         await interaction.channel.send(result_string)
 
-        if guess == game.get_word():
+        if guess == game.word:
             await interaction.followup.send("You win!")
             self.end_wordle_game(interaction.user.id)
             player_win_game(str(interaction.user.id))
             return
         
-        discarded_letters = game.get_discarded_letters()
+        discarded_letters = game.discarded_letters
         discarded_letters_list = list(discarded_letters)  # Convert the set to a list
         discarded_letters_string = ""
 
@@ -150,13 +127,13 @@ class WordleCog(commands.GroupCog, name="wordle"):
 
         await interaction.channel.send("Discarded letters: " +  discarded_letters_string)
         
-        game.subtract_guess()
+        game.guesses_left -= 1
 
-        await interaction.channel.send("Guesses left: " + str(game.get_guesses_left()))
+        await interaction.channel.send("Guesses left: " + str(game.guesses_left))
 
-        if game.get_guesses_left() == 0:
+        if game.guesses_left == 0:
             await interaction.channel.send("You lose!")
-            await interaction.channel.send("The word was: " + game.get_word().upper())
+            await interaction.channel.send("The word was: " + game.word.upper())
             self.end_wordle_game(interaction.user.id)
             await interaction.followup.send("Play again with /wordle.")
             player_lose_game(str(interaction.user.id))
@@ -169,7 +146,7 @@ class WordleCog(commands.GroupCog, name="wordle"):
     @app_commands.command(name="wordle_player_stats", description="Get the stats of a player.")
     @app_commands.describe(player="The player you want to get stats for.")
     @app_commands.guild_only()
-    async def wordle_player_stats(self, interaction: discord.Interaction, player: discord.User):
+    async def wordle_player_stats(self, interaction: discord.Interaction, player: discord.User) -> None:
         if player_in_stats(str(player.id)):
             wins = get_wins(str(player.id))
             losses = get_losses(str(player.id))
@@ -177,29 +154,54 @@ class WordleCog(commands.GroupCog, name="wordle"):
             await interaction.response.send_message(f"{player.display_name} has {wins} wins and {losses} losses.\n Win rate: {win_rate}%")
         else:
             await interaction.response.send_message(f"{player.display_name} has not played any wordle games")
+    
+
+    @app_commands.command(name='setup_database', description="Setup the wordle database.")
+    @app_commands.guild_only()
+    @app_commands.checks.has_permissions(ban_members=True)
+    @app_commands.default_permissions(ban_members=True)
+    async def setup_database(self, interaction: discord.Interaction) -> None:
+        setup_db()
+        await interaction.response.send_message("Database setup complete.")
 
 
-async def setup(bot: commands.Bot): 
+
+async def setup(bot: commands.Bot) -> None: 
     await bot.add_cog(WordleCog(bot))
 
 
 def get_valid_guesses():
-    file_path = get_base_path() + VALID_GUESSES_PATH
-    with open(file_path, 'r') as f:
-        valid_guesses = f.read().splitlines()
-        return valid_guesses
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
+    c = conn.cursor()
+
+    c.execute("SELECT word FROM valid_guesses")
+    valid_guesses = c.fetchall()
+
+    conn.close()
+
+    return valid_guesses
+
 
 
 def get_all_solutions():
-    file_path = get_base_path() + VALID_SOLUTIONS_PATH
-    with open(file_path, 'r') as f:
-        valid_solutions = f.read().splitlines()
-        return valid_solutions
+    # Get standard solutions
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
+
+    c = conn.cursor()
+
+    c.execute("SELECT word FROM standard_solutions")
+
+    standard_solutions = c.fetchall()
+
+    # Return solutions
+
+    return standard_solutions
+
 
 
 def check_guess(guess: str, game: wordle_game):
-    solution = game.get_word()
-    solution_length = game.get_word_length()
+    solution = game.word
+    solution_length = len(game.word)
     output = ["_" for _ in range(solution_length)]
     letters = list(solution)
 
@@ -216,28 +218,26 @@ def check_guess(guess: str, game: wordle_game):
         else:
             output[i] = "⬜"
             if guess[i] not in solution:
-                game.update_discarded_letters(guess[i])
+                game.discarded_letters.add(guess[i])
 
     output_string = "".join(output)
 
     return output_string
 
 
-def player_in_stats(user_id:str ):
-    file_path = get_base_path() + PLAYER_STATS_PATH
+def player_in_stats(user_id) -> bool:
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
+    c = conn.cursor()
 
-    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
-        print("File is empty or doesn't exist")
+    c.execute("SELECT user_id FROM player_stats WHERE user_id = ?", (user_id,))
+
+    result = c.fetchone()
+    conn.close()
+
+    if result is None:
         return False
-    
-    with open(file_path, 'r') as stats_file:
-        csv_reader = csv.reader(stats_file)
-
-        for row in csv_reader:
-            if row[0] == user_id:
-                return True
-
-        return False
+    else:
+        return True
     
 
 def player_win_game(user_id: str):
@@ -245,70 +245,172 @@ def player_win_game(user_id: str):
     if not player_in_stats(str(user_id)):
         add_player_to_stats(user_id)
     
-    # Update stats.csv and add 1 to second column
-    with open(get_base_path() + PLAYER_STATS_PATH, 'r') as stats_file:
-        csv_reader = csv.reader(stats_file)
-        lines = list(csv_reader)
-        for i in range(len(lines)):
-            if lines[i][0] == user_id:
-                lines[i][1] = int(lines[i][1]) + 1
-    
-    # Write to stats.csv
-    with open(get_base_path() + PLAYER_STATS_PATH, 'w') as stats_file:
-        csv_writer = csv.writer(stats_file)
-        csv_writer.writerows(lines)
+    # Update player_stats table and add 1 to games_won column
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
+    c = conn.cursor()
+
+    c.execute("SELECT games_won FROM player_stats WHERE user_id = ?", (user_id,))
+
+    result = c.fetchone()
+
+    if result is None:
+        return
+    else:
+        games_won = result[0]
+        games_won += 1
+        c.execute("UPDATE player_stats SET games_won = ? WHERE user_id = ?", (games_won, user_id))
+        conn.commit()
+        conn.close()
 
 
 def player_lose_game(user_id: str):
-    # If player is not in stats.csv, add them
-    if not player_in_stats(user_id):
+    # If player is not in player_stats table, add them
+    if not player_in_stats(str(user_id)):
         add_player_to_stats(user_id)
-    
-    # Update stats.csv and add 1 to third column
-    with open(get_base_path() + PLAYER_STATS_PATH, 'r') as stats_file:
-        csv_reader = csv.reader(stats_file)
-        lines = list(csv_reader)
-        for i in range(len(lines)):
-            if lines[i][0] == user_id:
-                lines[i][2] = int(lines[i][2]) + 1
-    
-    # Write to stats.csv
-    with open(get_base_path() + PLAYER_STATS_PATH, 'w') as stats_file:
-        csv_writer = csv.writer(stats_file)
-        csv_writer.writerows(lines)
+
+    # Update player_stats table and add 1 to games_lost column
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
+    c = conn.cursor()
+
+    c.execute("SELECT games_lost FROM player_stats WHERE user_id = ?", (user_id,))
+    result = c.fetchone()
+
+    if result is None:
+        return
+    else:
+        games_lost = result[0]
+        games_lost += 1
+        c.execute("UPDATE player_stats SET games_lost = ? WHERE user_id = ?", (games_lost, user_id))
+        conn.commit()
+        conn.close()
 
 
 def add_player_to_stats(user_id: str):
-    with open(get_base_path() + PLAYER_STATS_PATH, 'a') as stats_file:
-        csv_writer = csv.writer(stats_file)
-        csv_writer.writerow([user_id, 0, 0])
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
+
+    c = conn.cursor()
+
+    c.execute("INSERT INTO player_stats (user_id, games_played, games_won, games_lost, total_guesses, correct_guesses, incorrect_guesses) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, 0, 0, 0, 0, 0, 0))
 
 
 def get_wins(user_id: str):
-    with open(get_base_path() + PLAYER_STATS_PATH, 'r') as stats_file:
-        csv_reader = csv.reader(stats_file)
+    conn = sqlite3.connect(get_base_path() + '/db/wordle.db')
 
-        for row in csv_reader:
-            if row[0] == user_id:
-                return row[1]
+    c = conn.cursor()
+
+    c.execute("SELECT games_won FROM player_stats WHERE user_id = ?", (user_id,))
+
+    result = c.fetchone()
+    conn.close()
+
+    if result is None:
+        return None
+    else:
+        return result[0]
 
 
 def get_losses(user_id: str):
-    with open(get_base_path() + PLAYER_STATS_PATH, 'r') as stats_file:
-        csv_reader = csv.reader(stats_file)
+    conn = sqlite3.connect(get_base_path + '/db/wordle.db')
 
-        for row in csv_reader:
-            if row[0] == user_id:
-                return row[2]
+    c = conn.cursor()
+
+    c.execute("SELECT games_lost FROM player_stats WHERE user_id = ?", (user_id,))
+
+    result = c.fetchone()
+    conn.close()
+
+    if result is None:
+        return None
+    else:
+        return result[0]
 
 
 def get_win_percentage(user_id: str):
-    with open(get_base_path() + PLAYER_STATS_PATH, 'r') as stats_file:
-        csv_reader = csv.reader(stats_file)
+    conn = sqlite3.connect(get_base_path + '/db/wordle.db')
 
-        for row in csv_reader:
-            if row[0] == user_id:
-                if int(row[1]) + int(row[2]) == 0:
-                    return 0
-                else:
-                    return round(int(row[1]) / (int(row[1]) + int(row[2])) * 100, 2)
+    c = conn.cursor()
+
+    c.execute("SELECT games_won, games_lost FROM player_stats WHERE user_id = ?", (user_id,))
+
+    result = c.fetchone()
+    conn.close()
+
+    if result is None:
+        return None
+    else:
+        wins = result[0]
+        losses = result[1]
+        total_games = wins + losses
+
+        if total_games == 0:
+            return 0
+        else:
+            win_rate = (wins / total_games) * 100
+            return round(win_rate, 2)
+
+
+def setup_db():
+    conn = sqlite3.connect(get_base_path + '/db/wordle.db')
+
+    c = conn.cursor()
+
+    result = c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = result.fetchall()
+
+    for table in tables:
+        c.execute(f"DROP TABLE {table[0]}")
+
+    # Create solutions table
+    c.execute(""" CREATE TABLE standard_solutions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT
+            )""")
+
+    # Create a dune solutions table
+    c.execute(""" CREATE TABLE dune_solutions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT
+            )""")
+
+    c.execute(""" CREATE TABLE valid_guesses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT
+            )""")
+
+    # Create player stats table
+    c.execute(""" CREATE TABLE player_stats (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                games_played INTEGER,
+                games_won INTEGER,
+                games_lost INTEGER,
+                total_guesses INTEGER,
+                correct_guesses INTEGER,
+                incorrect_guesses INTEGER
+                )"""
+    )
+
+    # Add standard solutions to the standard solutions table
+    with open(VALID_SOLUTIONS_PATH, 'r') as file:
+        for line in file:
+            c.execute("INSERT INTO standard_solutions (word) VALUES (?)", (line.strip(),))
+
+    # Add dune solutions to the dune solutions table
+    with open(DUNE_SOLUTIONS_PATH, 'r') as file:
+        for line in file:
+            c.execute("INSERT INTO dune_solutions (word) VALUES (?)", (line.strip(),))
+
+    # Add valid guesses to the valid guesses table
+    with open(VALID_GUESSES_PATH, 'r') as file:
+        for line in file:
+            c.execute("INSERT INTO valid_guesses (word) VALUES (?)", (line.strip(),))
+
+    # Add player stats to the player stats table
+    with open(PLAYER_STATS_PATH, 'r') as file:
+        for line in file:
+            user_id, games_won, games_lost = line.strip().split(',')
+            c.execute("INSERT INTO player_stats (user_id, games_won, games_lost) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, 0, games_won, games_lost, 0, 0, 0))
+
+
+    conn.commit()
+    conn.close()
