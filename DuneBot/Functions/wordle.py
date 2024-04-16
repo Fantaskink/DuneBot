3e2fa852d7a98f77080ae7f7acdf4b80c88ed2fd
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-04-16 22:40:22 +0200

    Update wordle.py

--- DuneBot/Functions/wordle.py
+++ DuneBot/Functions/wordle.py
@@ -413,7 +413,7 @@ def setup_db() -> None:
             user_id = int(user_id)
             games_won = int(games_won)
             games_lost = int(games_lost)
-            c.execute("INSERT INTO player_stats (user_id, games_won, games_lost) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, 0, games_won, games_lost, 0, 0, 0))
+            c.execute("INSERT INTO player_stats (user_id, games_played, games_won, games_lost, total_guesses, correct_guesses, incorrect_guesses) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, 0, games_won, games_lost, 0, 0, 0))
 
 
     conn.commit()
