a12bd8472238a8fe3b55b9d3d4b3d743730bfec2
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-01-24 19:00:09 +0100

    Update nitro.py

--- DuneBot/Functions/nitro.py
+++ DuneBot/Functions/nitro.py
@@ -23,11 +23,7 @@ class NitroCog(commands.Cog):
     @app_commands.guild_only()
     @app_commands.checks.has_permissions(ban_members=True)
     @app_commands.default_permissions(ban_members=True)
-    async def upload_boosters(self, interaction: discord.Interaction) -> None:
-        if not interaction.user.guild_permissions.administrator:
-            await interaction.response.send_message("You are not authorized to run this command.", ephemeral=True)
-            return
-        
+    async def upload_boosters(self, interaction: discord.Interaction) -> None:        
         with open(BOOSTER_CSV_PATH, 'r') as stats_file:
             csv_reader = csv.reader(stats_file)
             rows = list(csv_reader)
