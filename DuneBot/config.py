8ff6950275664175e473a07bc39ed63825ee7c38
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-01-31 14:19:15 +0100

    Update config.py

--- DuneBot/config.py
+++ DuneBot/config.py
@@ -10,19 +10,12 @@ ENVIRONMENT = os.environ.get("ENVIRONMENT")
 MODLOG_CHANNEL_ID = int(os.environ.get("MODLOG_CHANNEL_ID"))
 TOKEN = os.environ.get("TOKEN")
 PREFIX = "!"
-PRODUCTION_BASE_PATH = '/home/andreas/DiscordBots/DuneBot/DuneBot/'
-DEVELOPMENT_BASE_PATH = 'DuneBot/'
 
 OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
 
 DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
-
 DB_NAME = os.environ.get("DB_NAME")
-
 db_client = MongoClient(DB_CONNECTION_STRING, server_api=ServerApi('1'))[DB_NAME]
 
 def get_base_path():
-    if ENVIRONMENT == "production":
-        return PRODUCTION_BASE_PATH
-    elif ENVIRONMENT == "development":
-        return DEVELOPMENT_BASE_PATH
+    return os.path.dirname(os.path.abspath(__file__)) + "/"
